c35b7a3633bc4177c82db3bc6fd0bf10935f786b
kiranrathor30697 <kiranrathor30697@gmail.com>
2022-01-20 12:04:57 -1100

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+//import Area
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//1. state/variable
+const [friendName, setFriendName] = useState('kiran');
+
+//2. function
+let myFunction = () =>{
+  console.log(friendName)
 }
 
+//3. return statement
+
+return (
+  <div className="App">
+    <form>
+      <label>Enter Friends Name:<br />
+        <input type="text" value={ friendName } onChange={(e)=>{setFriendName(e.target.value)}} />
+      </label><br />
+      <input type="button" onClick={myFunction} name="Friends Name" value="Save Friends" />
+    </form>
+    
+  </div>
+);
+}
 export default App;
