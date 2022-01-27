5a9a445f986438a1db4ed651a889d8ab79d9f3f2
kiranrathor30697 <kiranrathor30697@gmail.com>
2022-01-26 23:32:50 -1100

    create student

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 //import Area
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 //1. state/variable
-const [friendName, setFriendName] = useState('kiran');
+const [studentName, setStudentName] = useState('');
+
+useEffect(()=>{
+
+}, [] );
+
 
 //2. function
 let myFunction = () =>{
-  console.log(friendName)
+  console.log(studentName);
+  //call the api here
+  var data = {
+    "data":{
+      "name":studentName
+    }
+  }
+  //fetch api/promise chain
+  fetch("http://localhost:1337/api/students",{
+    method:'POST',
+    headers:{
+      //'accept': 'application/json',
+      'Content-Type':'application/json',
+    },
+    body:JSON.stringify(data)
+
+  })
+  //.then(Response => Response.json())
+
+  .then(()=>{
+    //console.log(data)
+  })
+
+  .catch(()=>{
+    //console.log(error)
+  })
+  
+  .finally(()=>{
+    //console.log(all)
+  });
 }
 
 //3. return statement
 
 return (
   <div className="App">
+    { studentName }
     <form>
-      <label>Enter Friends Name:<br />
-        <input type="text" value={ friendName } onChange={(e)=>{setFriendName(e.target.value)}} />
-      </label><br />
-      <input type="button" onClick={myFunction} name="Friends Name" value="Save Friends" />
+      <label>Enter student Name:  
+        <input type="text" value={ studentName } name='studentName' onChange={(e)=>{ setStudentName(e.target.value)} } /><br />
+      </label>
+      
+      <input type="button" onClick={ myFunction } name="studentName" value="Save student" />
     </form>
     
   </div>
